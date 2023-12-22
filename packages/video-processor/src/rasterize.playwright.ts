70d19e50faefc8c114271c7d30cb56d4d364c42c
David Turissini <davidturissini@Melissas-Air.home>
2023-12-21 16:04:11 -0800

    simplifying tests

--- packages/video-processor/src/rasterize.playwright.ts
+++ packages/video-processor/src/rasterize.playwright.ts
@@ -49,105 +49,6 @@ async function assertVideo({ durationSeconds, videoLocator, page, snapshot: { ti
 }
 
 test.describe('rasterizeDocument', () => {
-  test.describe(
-    'single CSS clip',
-    () => {
-      test('should rasterize with correct duration', async ({ page }) => {
-        await page.goto('/');
-        await page.evaluate(async () => {
-          const result = await window.animationClipToImageSequence({
-            clip: {
-              source: 'css',
-              id: 'clip',
-              win: {
-                startMilliseconds: 0,
-                durationMilliseconds: 1000,
-              },
-            },
-            source: {
-              durationMilliseconds: 1000,
-              thumbnail: {
-                url: 'thumbnail',
-                data: new Blob(),
-                originalDevicePixelRatio: 1,
-                originalDimensions: {
-                  width: 10,
-                  height: 10,
-                },
-              },
-              id: 'css',
-              type: 'animation',
-              title: 'Untitled',
-              html: `
-              <div class="ball"><div>
-            `,
-              css: `
-              .ball {
-                height: 900px;
-                width: 1600px;
-                background: red;
-                animation: move-ball 1s;
-              }
-
-              @keyframes move-ball {
-                0% {
-                  opacity: 0;
-                }
-
-                100% {
-                  opacity: 1;
-                }
-              }
-            `,
-            },
-            doc: {
-              frameRate: 30,
-              dimensions: {
-                height: 900,
-                width: 1600,
-              },
-              durationMilliseconds: 1000,
-              timeline: [
-                {
-                  source: 'css',
-                  id: 'clip',
-                  win: {
-                    startMilliseconds: 0,
-                    durationMilliseconds: 1000,
-                  },
-                },
-              ],
-            },
-          });
-
-          async function renderVideFile({ url }: VideoSource) {
-            const vid = document.createElement('video');
-            vid.title = 'output-video';
-            const durationPromise = new Promise<void>((res) => {
-              vid.addEventListener('durationchange', () => res(), {
-                once: true,
-              });
-            });
-
-            vid.src = url;
-            document.body.appendChild(vid);
-
-            await durationPromise;
-          }
-
-          await renderVideFile(result);
-        });
-
-        const outputVideo = page.getByTitle('output-video');
-        await expect(outputVideo).toHaveCount(1);
-        const duration = await outputVideo.evaluate(
-          (el: HTMLVideoElement) => el.duration,
-        );
-        expect(duration).toBeCloseTo(1, 2);
-      });
-    },
-  );
-
   test.describe('Snapshots', () => {
     test('basic counting video', async ({ page }) => {
       await page.goto('/');
@@ -279,42 +180,24 @@ test.describe('rasterizeDocument', () => {
         await renderVideFile(result);
       });
 
-      const outputVideo = page.getByTitle('output-video');
-      await expect(outputVideo).toHaveCount(1);
-      await page.evaluate(async () => {
-        const vid = document.getElementById('vid') as HTMLVideoElement;
-        vid.currentTime = 1;
+      await assertVideo({
+        durationSeconds: 5,
+        videoLocator: page.getByTitle('output-video'),
+        page,
+        snapshot: {
+          timestamps: [{
+            seconds: 1
+          }, {
+            seconds: 2
+          }, {
+            seconds: 3
+          }, {
+            seconds: 4
+          }, {
+            seconds: 5
+          }]
+        }
       });
-
-      await expect(outputVideo).toHaveScreenshot({});
-
-      await page.evaluate(async () => {
-        const vid = document.getElementById('vid') as HTMLVideoElement;
-        vid.currentTime = 2;
-      });
-
-      await expect(outputVideo).toHaveScreenshot();
-
-      await page.evaluate(async () => {
-        const vid = document.getElementById('vid') as HTMLVideoElement;
-        vid.currentTime = 3;
-      });
-
-      await expect(outputVideo).toHaveScreenshot();
-
-      await page.evaluate(async () => {
-        const vid = document.getElementById('vid') as HTMLVideoElement;
-        vid.currentTime = 4;
-      });
-
-      await expect(outputVideo).toHaveScreenshot();
-
-      await page.evaluate(async () => {
-        const vid = document.getElementById('vid') as HTMLVideoElement;
-        vid.currentTime = 5;
-      });
-
-      await expect(outputVideo).toHaveScreenshot();
     });
 
     test('simple video', async ({ page }) => {
