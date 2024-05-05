d6f1f888dd121c7d2919b4d7df6d39fe0c32d1e7
David Turissini <davidturissini@Melissas-Air.home>
2024-05-04 23:16:55 -0700

    walking virtual file system

--- crates/ts-garbage-collector/src/main.rs
+++ crates/ts-garbage-collector/src/main.rs
@@ -1,5 +1,6 @@
 use std::path::{Path, PathBuf};
 use path_clean::clean;
+use vfs::VfsPath;
 use swc_common::{
   sync::Lrc,
   FileName, SourceMap,
@@ -10,8 +11,7 @@ use swc_ecma_parser::{lexer::Lexer, Capturing, Parser, StringInput, Syntax};
 
 
 fn main() {
-  let tokens = parse("/path/to/test.ts", "import { foo } from './somewhere';");
-  let sources = get_import_sources("/path/to/test.ts", tokens);
+  
 }
 
 
@@ -40,6 +40,33 @@ pub fn parse(filepath: &str, source: &str) -> Module {
       .expect("Failed to parse module.");
 }
 
+fn collect_source_files(path: VfsPath) -> Vec<PathBuf> {
+  let mut files: Vec<PathBuf> = Vec::new();
+  
+  match path.read_dir() {
+    Ok(results) => {
+      for result in results {
+        match result.is_dir() {
+          Ok(is_dir) => {
+            if is_dir == true {
+              let dir_files = collect_source_files(result);
+              for d in dir_files {
+                files.push(d);
+              }
+            } else {
+              files.push(Path::new(result.as_str()).join(""));
+            }
+          },
+          Err(_) => {}
+        }
+        
+      }
+    },
+    Err(_) => {  }
+  }
+
+  return files;
+}
 
 fn get_import_sources(filename: &str, module: Module) -> Vec<PathBuf> {
   struct ImportSources<'a> {
@@ -67,13 +94,33 @@ fn get_import_sources(filename: &str, module: Module) -> Vec<PathBuf> {
 }
 
 #[cfg(test)]
-mod tests {
+mod get_import_sources_tests {
     use super::*;
 
     #[test]
     fn it_should_return_import_paths() {
       let parsed = parse("test.ts", "import { foo } from './somewhere';");
       let result = get_import_sources("/path/to/test.ts", parsed);
-      assert_eq!(result, vec![Path::new("/path/to/somewhere.ts")]);
+      assert_eq!(result, vec![Path::new("/path/to/somewhere.ts").join("")]);
     }
+}
+
+#[cfg(test)]
+mod collect_source_files_tests {
+  use vfs::MemoryFS;
+
+use super::*;
+
+  #[test]
+  fn it_should_return_expected_paths() {
+    let root: VfsPath = MemoryFS::new().into();
+    
+    let path = root.join("/path/to/src").unwrap();
+    let _ = path.create_dir_all();
+    let _ = root.join("/path/to/src/test.ts").unwrap().create_file();
+    
+    assert_eq!(
+      collect_source_files(root), vec![Path::new("/path/to/src/test.ts")]
+    )
+  }
 }
